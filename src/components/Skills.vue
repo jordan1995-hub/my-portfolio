060dbc727b19eb1855e23a7d9720fbe6587b46e4
unknown <canetedanz06@gmail.com>
2024-03-02 13:30:19 +0800

    deleting all public

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -23,13 +23,13 @@ import { ref } from 'vue'
 const title = ref('My Skills')
 const skills = ref([
   {name:'HTML', icon: "/assets/logo5.png"}, 
-  {name:'CSS', icon: "/public/assets/logo6.png"},
-  {name:'JAVASCRIPT', icon: "/public/assets/logo4.png"},
-  {name:'TAILWIND CSS', icon: "/public/assets/logo7.png"},
-  {name:'GIT', icon: "/public/assets/logo3.png"},
-  {name:'VUE JS', icon: "/public/assets/logo9.png"},
-  {name:'UI/UX DESIGN', icon: "/public/assets/logo2.png"},
-  {name:'WORDPRESS ', icon: "/public/assets/logo1.png"},
+  {name:'CSS', icon: "/assets/logo6.png"},
+  {name:'JAVASCRIPT', icon: "/assets/logo4.png"},
+  {name:'TAILWIND CSS', icon: "/assets/logo7.png"},
+  {name:'GIT', icon: "/assets/logo3.png"},
+  {name:'VUE JS', icon: "/assets/logo9.png"},
+  {name:'UI/UX DESIGN', icon: "/assets/logo2.png"},
+  {name:'WORDPRESS ', icon: "/assets/logo1.png"},
   
 ])
 </script>
